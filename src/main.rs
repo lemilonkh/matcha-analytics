9a659a1edf717783156391fcdf7e6a951253874f
Milan Gruner <milangruner@gmail.com>
2024-03-10 00:02:00 +0100

    Bind to random port in integration test to prevent collision with running server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use matcha_analytics::run;
+use std::net::TcpListener;
+
+const SERVER_URL: &str = "127.0.0.1:8000";
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind(SERVER_URL)?;
+    println!("Listening on http://{SERVER_URL}");
+    run(listener)?.await
 }
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,16 @@
+use std::net::TcpListener;
+
 use matcha_analytics::run;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -18,7 +20,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    // port 0 searches for an available port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
     std::mem::drop(tokio::spawn(server));
+
+    format!("http://127.0.0.1:{}", port)
 }
