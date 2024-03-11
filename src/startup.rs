0dc33d3cc758d05bda8ab697ebd5e7c7e0cdfbd2
Milan Gruner <milangruner@gmail.com>
2024-03-11 14:25:49 +0100

    Remove unused import from startup

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
