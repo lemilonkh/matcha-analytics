6b86fb2bc291fa2408021a8b5040615c59cec0b8
Milan Gruner <milangruner@gmail.com>
2024-03-13 00:34:18 +0100

    Extract telemetry module to reuse in tests and add once_cell crate

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use matcha_analytics::{configuration::get_configuration, startup::run};
+use matcha_analytics::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // logging setup with tracing
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("matcha_analytics".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set logging subscriber");
+    let subscriber = get_subscriber("matcha_analytics".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,9 +3,16 @@ use std::net::TcpListener;
 use matcha_analytics::{
     configuration::{get_configuration, DatabaseSettings},
     startup::run,
+    telemetry::{get_subscriber, init_subscriber},
 };
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber = get_subscriber("test".into(), "debug".into());
+    init_subscriber(subscriber);
+});
 
 pub struct TestApp {
     pub address: String,
@@ -14,6 +21,8 @@ pub struct TestApp {
 
 /// Start app on a random unused port and return its address
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     // port 0 searches for an available port
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
