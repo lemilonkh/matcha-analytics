8a884493733b55f21bb7e5aa732eacd3c9a5d0bc
Milan Gruner <milangruner@gmail.com>
2024-03-13 00:22:56 +0100

    Use LogTracer to emit normal log messages via tracing

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // logging setup with tracing
+    LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("matcha_analytics".into(), std::io::stdout);
     let subscriber = Registry::default()
