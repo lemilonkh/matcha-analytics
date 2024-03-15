aaca2298a81b85288e3b12326208324621b464ac
Milan Gruner <milangruner@gmail.com>
2024-03-15 22:23:16 +0100

    Properly handle validation errors instead of crashing process

--- src/domain.rs
+++ src/domain.rs
@@ -5,21 +5,22 @@ pub struct NewSubscriber {
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
     /// Returns SubscriberName instance if input satisfies all constraints
-    pub fn parse(s: String) -> SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
         let is_too_long = s.graphemes(true).count() > 256;
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            panic!("{} is not a valid subscriber name.", s);
+            Err(format!("{} is not a valid subscriber name.", s))
+        } else {
+            Ok(Self(s))
         }
-
-        Self(s)
     }
     pub fn inner(&self) -> &str {
         &self.0
@@ -29,5 +30,44 @@ impl SubscriberName {
 impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ё".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_and_only_whitespace_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+        let name = " \n  ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            assert_err!(SubscriberName::parse(name.to_string()));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Tester Testerino".to_string();
+        assert_ok!(SubscriberName::parse(name));
     }
 }
