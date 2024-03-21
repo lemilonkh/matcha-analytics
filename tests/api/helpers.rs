28a2e87e5251aff6b28ef9a67f5956c96f0256f8
Milan Gruner <milangruner@gmail.com>
2024-03-21 22:50:41 +0100

    Return port in TestApp struct from spawn_app()

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -25,6 +25,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
 }
@@ -64,11 +65,13 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build application");
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
+    let address = format!("http://127.0.0.1:{}", application_port);
     std::mem::drop(tokio::spawn(application.run_until_stopped()));
 
     TestApp {
         address,
+        port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
     }
