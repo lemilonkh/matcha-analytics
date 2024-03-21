3b2edc6bef0601065e3206d62247f7768ea52bb8
Milan Gruner <milangruner@gmail.com>
2024-03-21 22:53:32 +0100

    Add application.base_url to configuration

--- src/startup.rs
+++ src/startup.rs
@@ -39,7 +39,12 @@ impl Application {
         );
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool, email_client)?;
+        let server = run(
+            listener,
+            connection_pool,
+            email_client,
+            configuration.application.base_url
+        )?;
         println!("Listening on http://{address}");
 
         Ok(Self { port, server })
@@ -58,18 +63,24 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPool::connect_lazy_with(configuration.with_db())
 }
 
+// context retrieval in actix-web is type based
+pub struct ApplicationBaseUrl(pub String);
+
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: String,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool); // wrap in Arc
     let email_client = Data::new(email_client);
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(base_url.clone())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
